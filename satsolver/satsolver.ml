5cb7d707534eb3f204571c8b924f68f9035c11fe
gaspard <henault.gaspard.tls@gmail.com>
2026-04-18 15:08:43 +0200

    Q2

--- satsolver/satsolver.ml
+++ satsolver/satsolver.ml
@@ -4,9 +4,15 @@ let test () =
 
 let main () = 
   test();
+  if (Array.length Sys.argv) = 1 then 
+    failwith "Aucun argument"
+  else if (Sys.argv.(1) = "test") then 
+    test()
+  else
   print_int (Array.length Sys.argv); print_string "\n\n";
   print_string Sys.argv.(0); print_string "\n\n";
   print_string Sys.argv.(1); print_string "\n\n"
 
 
 let _ = main() (* Exécution de la fonction main *)
+
