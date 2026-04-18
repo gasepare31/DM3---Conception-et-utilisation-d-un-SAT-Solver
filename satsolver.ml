ebe3fc0de4173e303cbc11c3e4c4bc211dfca7ec
gaspard <henault.gaspard.tls@gmail.com>
2026-04-18 14:51:04 +0200

    Update satsolver.ml

--- satsolver.ml
+++ satsolver.ml
@@ -1,3 +1,12 @@
 let test () = 
   assert (1=1);
   print_string "Tous les tests ont réussi\n"
+
+let main () = 
+  test();
+  print_int (Array.length Sys.argv); print_string "\n\n";
+  print_string Sys.argv.(0); print_string "\n\n";
+  print_string Sys.argv.(1); print_string "\n\n"
+
+
+let _ = main() (* Exécution de la fonction main *)
