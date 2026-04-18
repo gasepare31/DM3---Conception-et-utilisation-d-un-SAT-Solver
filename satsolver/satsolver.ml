ab49481b3acbf359866872ed4f36d68ba425f39e
gaspard <henault.gaspard.tls@gmail.com>
2026-04-18 15:13:51 +0200

    Q3

--- satsolver/satsolver.ml
+++ satsolver/satsolver.ml
@@ -11,7 +11,11 @@ let main () =
   else
   print_int (Array.length Sys.argv); print_string "\n\n";
   print_string Sys.argv.(0); print_string "\n\n";
-  print_string Sys.argv.(1); print_string "\n\n"
+  print_string Sys.argv.(1); print_string "\n\n";
+  let ic = open_in (Sys.argv.(1)) in 
+  let res = input_line ic in 
+  close_in ic; 
+  print_string res
 
 
 let _ = main() (* Exécution de la fonction main *)
